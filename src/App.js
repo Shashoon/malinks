06b1c3aa3ce1c626a663ac3b19ccb1153aa48e55
SharonMazcka <35423750+sharonMazcka@users.noreply.github.com>
2022-12-06 16:26:43 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Links from './Links';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Header />
+
+        <div className='container'>
+
+          <div className='image-container'>
+            <img src={require('./profile.jpg')} alt='User Avatar' />
+          </div>
+
+          <h1>Sharon Mazcka</h1>
+
+          <Links />
+
+          <div className='footer'>@Malinks</div>
+
+        </div>
+      </div>
+    </>
   );
 }
 
